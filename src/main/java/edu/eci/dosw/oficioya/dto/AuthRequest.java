bcfad5112462c6b3257532fb3f0ac9a0998d7904
Santiago <daniel.ibanez-g@mail.escuelaing.edu.co>
2026-09-24 19:38:05 -0500

    fix(first_cicle): correccion de error al hacer merge

---
+++ src/main/java/edu/eci/dosw/oficioya/dto/AuthRequest.java
@@ -0,0 +1,20 @@
+package edu.eci.dosw.oficioya.dto;
+
+public class AuthRequest {
+
+    private String correo;
+    private String contrasena;
+
+    public AuthRequest() {}
+
+    public AuthRequest(String correo, String contrasena) {
+        this.correo = correo;
+        this.contrasena = contrasena;
+    }
+
+    public String getCorreo() { return correo; }
+    public void setCorreo(String correo) { this.correo = correo; }
+
+    public String getContrasena() { return contrasena; }
+    public void setContrasena(String contrasena) { this.contrasena = contrasena; }
+}
